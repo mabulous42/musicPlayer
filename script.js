52869d08909f83d90c815f8eff783d4ca60a6ff7
Abbas <tundeeey@gmail.com>
2023-04-18 23:13:43 +0100

    the play button now triggers the music at first click and the pause button also pause the music at first click too

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ let playMusic = document.getElementById("play");
 let pauseMusic = document.getElementById("pause");
 
 // pauseMusic.style.display = "none";
-// playMusic.style.display = "none";
+playMusic.style.display = "none";
 function playSong() {
     audioFile.play();
-    pauseMusic.style.display = "block";
-    playMusic.style.display = "none";
+    pauseMusic.style.display = "none";
+    playMusic.style.display = "block";
 }
 
 function pauseSong() {
     audioFile.pause();
-    playMusic.style.display = "block";
-    pauseMusic.style.display = "none";
+    playMusic.style.display = "none";
+    pauseMusic.style.display = "block";
 }
 audioFile.volume = 0.5;
 // console.log(volumeLevel);
